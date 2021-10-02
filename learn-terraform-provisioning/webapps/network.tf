e7e1d8a0f8617078b88724cf8b37dee67fe294d0
nasohi <ciptandani@gmail.com>
2021-10-02 13:35:34 +0700

    modified:   network.tf

--- learn-terraform-provisioning/webapps/network.tf
+++ learn-terraform-provisioning/webapps/network.tf
@@ -59,6 +59,14 @@ resource "aws_security_group" "nshcloudlabs-public-security" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   
   egress {
     from_port   = 0
